3d0f25b2e0cbc701c949dd604a8125f094ee6b70
Imad Dabligi <imad.dabligi@gmail.com>
2024-01-27 14:45:02 +0100

    Connecting WP with Mariadb f

--- srcs/requirements/wordpress/wp.sh
+++ srcs/requirements/wordpress/wp.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# cd wp && wp config --dbhost=localhost --dbname=WP_db --dbuser=ST --dbpass=123 --allow-root
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
-#sleep 5
+chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp
+
+mkdir wp && cd wp && wp core download --allow-root
+
+mv wp-config-sample.php wp-config.php && wp config set SERVER_PORT 3306 --allow-root
 
 apt install iproute2 -y
-
 apt-get install iputils-ping
 
 wp config set DB_NAME 'WP_DB' --allow-root --path=/wp
@@ -14,6 +17,5 @@ wp config set DB_PASSWORD '123' --allow-root --path=/wp
 wp config set DB_HOST 'mariactr:3306' --allow-root --path=/wp
 
 wp core install --url=localhost --title=TEST --admin_user=king --admin_password=kingpass --admin_email=projectnum05@gmail.com --allow-root --path=/wp
-
 
 sleep 400000
