c976057a460c6c49c834b4d82316b915a4b59c76
Imad Dabligi <imad.dabligi@gmail.com>
2024-02-04 15:19:24 +0100

    Setting password for mariadb root

--- srcs/requirements/mariadb/database.sh
+++ srcs/requirements/mariadb/database.sh
@@ -4,17 +4,18 @@ mysqld_safe &
 
 sleep 5
 
-mariadb -u root<<EOF
+mariadb -u root <<EOF
 CREATE DATABASE WP_DB;
 CREATE USER 'WP'@'%' IDENTIFIED BY '123';
 GRANT ALL PRIVILEGES ON *.* TO 'WP'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-mysqladmin shutdown
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '911';"
+
+mysqladmin -u root -p911 shutdown
 
 mysqld_safe
-
 
 #The lines between <<EOF and EOF are executed as commands 
 # within the MariaDB shell.
