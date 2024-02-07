8e9479ae08fd7c48b6b2986a7fc8fc097934d411
Imad Dabligi <imad.dabligi@gmail.com>
2024-02-07 11:19:43 +0100

    Fixing WP_Files path

--- srcs/requirements/wordpress/wp.sh
+++ srcs/requirements/wordpress/wp.sh
@@ -4,19 +4,19 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 
 chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp
 
-chown -R root:root /wp
+chown -R root:root /var/www/html
 
 mkdir /run/php
 
-cd wp && wp core download --allow-root
+cd /var/www/html && wp core download --allow-root
 
 mv wp-config-sample.php wp-config.php && wp config set SERVER_PORT 3306 --allow-root
 
-wp config set DB_NAME 'WP_DB' --allow-root --path=/wp
-wp config set DB_USER 'WP' --allow-root --path=/wp
-wp config set DB_PASSWORD '123' --allow-root --path=/wp
-wp config set DB_HOST 'mactr:3306' --allow-root --path=/wp
+wp config set DB_NAME 'WP_DB' --allow-root --path=/var/www/html
+wp config set DB_USER 'WP' --allow-root --path=/var/www/html
+wp config set DB_PASSWORD '123' --allow-root --path=/var/www/html
+wp config set DB_HOST 'mactr:3306' --allow-root --path=/var/www/html
 
-wp core install --url=localhost --title=TEST --admin_user=king --admin_password=kingpass --admin_email=projectnum05@gmail.com --allow-root --path=/wp
+wp core install --url=localhost --title=TEST --admin_user=king --admin_password=kingpass --admin_email=projectnum05@gmail.com --allow-root --path=/var/www/html
 
 /usr/sbin/php-fpm7.4 -F
