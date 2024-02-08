1a192149de6d5c75136fffb086b10ff12f78432e
Imad Dabligi <imad.dabligi@gmail.com>
2024-02-08 15:15:40 +0100

    Creating MariaDB volume

--- srcs/requirements/wordpress/wp.sh
+++ srcs/requirements/wordpress/wp.sh
@@ -8,8 +8,6 @@ chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp
 
 mkdir /run/php
 
-# mkdir -p /var/www/html
-
 chown -R root:root /var/www/html
 
 cd /var/www/html && wp core download --allow-root
@@ -19,10 +17,10 @@ mv wp-config-sample.php wp-config.php && wp config set SERVER_PORT 3306 --allow-
 wp config set DB_NAME 'WP_DB' --allow-root --path=/var/www/html
 wp config set DB_USER 'WP' --allow-root --path=/var/www/html
 wp config set DB_PASSWORD '123' --allow-root --path=/var/www/html
-wp config set DB_HOST 'mactr:3306' --allow-root --path=/var/www/html
+wp config set DB_HOST 'mariadb:3306' --allow-root --path=/var/www/html
 
-wp core install --url=localhost --title=TEST --admin_user=king --admin_password=kingpass --admin_email=projectnum05@gmail.com --allow-root --path=/var/www/html
+wp core install --url=localhost --title=INCEPTION --admin_user=king --admin_password=kingpass --admin_email=projectnum05@gmail.com --allow-root --path=/var/www/html
 
-wp user create ahmed ahmed.folan@gmail.com --role=author --allow-root --path=/var/www/html --user_pass=ahmedpass
+wp user create ahmed ahmed.folan@gmail.com --role=author --user_pass=ahmedpass --allow-root --path=/var/www/html
 
 /usr/sbin/php-fpm7.4 -F
